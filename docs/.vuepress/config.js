ce93630045ea1b695406760f9c40f24e9eef742d
ailjc <lujingce@163.com>
2021-11-29 10:44:58 +0800

    rollback

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-function getSidebar2(sidebarPath) {
-  const menuPath = path.join(process.cwd(), 'docs', sidebarPath);
+function getSidebar(sidebarPath) {
+  const menuPath = path.join(process.cwd(), "docs", sidebarPath);
   const curPath = fs.readdirSync(menuPath);
   const sidebars = [];
-  curPath.forEach((item) => {
+  curPath.forEach(item => {
     if (
-      item.toLocaleLowerCase() !== 'readme.md' &&
-      item !== '.DS_Store' &&
-      item.indexOf('.') !== 0
+      item.toLocaleLowerCase() !== "readme.md" &&
+      item !== ".DS_Store" &&
+      item.indexOf(".") !== 0
     ) {
-      let curSidebar = item.replace('.md', '');
-      if (item.indexOf('.md') < 0) {
+      let curSidebar = item.replace(".md", "");
+      if (item.indexOf(".md") < 0) {
         curSidebar = {
           title: item,
-          path: encodeURI(`/${sidebarPath}/${item}/`),
+          path: `/${sidebarPath}/${item}/`
         };
         const childrenPath = fs.readdirSync(
-          path.join(process.cwd(), 'docs', sidebarPath, item)
+          path.join(process.cwd(), "docs", sidebarPath, item)
         );
         const curSidebarChildren = [];
-        childrenPath.forEach((childrenItem) => {
+        childrenPath.forEach(childrenItem => {
           const menuPath2 = path.join(
             process.cwd(),
-            'docs',
+            "docs",
             sidebarPath,
             item,
             childrenItem
@@ -32,38 +32,37 @@ function getSidebar2(sidebarPath) {
 
           if (!fs.lstatSync(menuPath2).isDirectory()) {
             if (
-              childrenItem.toLocaleLowerCase() !== 'readme.md' &&
-              childrenItem.indexOf('.md') >= 0 &&
-              item.indexOf('.') !== 0
+              childrenItem.toLocaleLowerCase() !== "readme.md" &&
+              childrenItem.indexOf(".md") >= 0 &&
+              item.indexOf(".") !== 0
             ) {
               curSidebarChildren.push({
-                title: childrenItem.replace('.md', ''),
-                path: encodeURI(
-                  `/${sidebarPath}/${item}/${childrenItem.replace('.md', '')}`
-                ),
+                title: childrenItem.replace(".md", ""),
+                path: `/${sidebarPath}/${item}/${childrenItem.replace(
+                  ".md",
+                  ""
+                )}`
               });
             }
           } else {
             let curSidebar = {
               title: childrenItem,
-              path: encodeURI(`/${sidebarPath}/${item}/${childrenItem}/`),
+              path: `/${sidebarPath}/${item}/${childrenItem}/`
             };
             let curMenu = [];
             const childrenPath2 = fs.readdirSync(menuPath2);
-            childrenPath2.forEach((childrenItem2) => {
+            childrenPath2.forEach(childrenItem2 => {
               if (
-                childrenItem2.toLocaleLowerCase() !== 'readme.md' &&
-                childrenItem2.indexOf('.md') >= 0 &&
-                childrenItem.indexOf('.') !== 0
+                childrenItem2.toLocaleLowerCase() !== "readme.md" &&
+                childrenItem2.indexOf(".md") >= 0 &&
+                childrenItem.indexOf(".") !== 0
               ) {
                 curMenu.push({
-                  title: childrenItem2.replace('.md', ''),
-                  path: encodeURI(
-                    `/${sidebarPath}/${item}/${childrenItem}/${childrenItem2.replace(
-                      '.md',
-                      ''
-                    )}`
-                  ),
+                  title: childrenItem2.replace(".md", ""),
+                  path: `/${sidebarPath}/${item}/${childrenItem}/${childrenItem2.replace(
+                    ".md",
+                    ""
+                  )}`
                 });
               }
             });
@@ -74,55 +73,9 @@ function getSidebar2(sidebarPath) {
         curSidebar.children = curSidebarChildren;
       } else {
         curSidebar = {
-          title: item.replace('.md', ''),
-          path: encodeURI(`/${sidebarPath}/${item.replace('.md', '')}`),
+          title: item.replace(".md", ""),
+          path: `/${sidebarPath}/${item.replace(".md", "")}`
         };
-      }
-      sidebars.push(curSidebar);
-    }
-  });
-  console.log(11111111);
-  console.log(sidebars);
-  console.log(22222222);
-  return sidebars;
-}
-
-function getSidebar(sidebarPath) {
-  const menuPath = path.join(process.cwd(), 'docs', sidebarPath);
-  const curPath = fs.readdirSync(menuPath);
-  const sidebars = [];
-  curPath.forEach((item) => {
-    if (
-      item.toLocaleLowerCase() !== 'readme.md' &&
-      item !== '.DS_Store' &&
-      item.indexOf('.') !== 0
-    ) {
-      let curSidebar = item.replace('.md', '');
-      if (item.indexOf('.md') < 0) {
-        curSidebar = {
-          title: item,
-          path: `/${sidebarPath}/${item}/`,
-        };
-        const childrenPath = fs.readdirSync(
-          path.join(process.cwd(), 'docs', sidebarPath, item)
-        );
-        const curSidebarChildren = [];
-        childrenPath.forEach((childrenItem) => {
-          if (
-            childrenItem.toLocaleLowerCase() !== 'readme.md' &&
-            childrenItem.indexOf('.md') >= 0 &&
-            item.indexOf('.') !== 0
-          ) {
-            curSidebarChildren.push({
-              title: childrenItem.replace('.md', ''),
-              path: `/${sidebarPath}/${item}/${childrenItem.replace(
-                '.md',
-                ''
-              )}`,
-            });
-          }
-        });
-        curSidebar.children = curSidebarChildren;
       }
       sidebars.push(curSidebar);
     }
@@ -131,17 +84,17 @@ function getSidebar(sidebarPath) {
 }
 
 module.exports = {
-  title: 'Ailjc notes',
-  description: ' ',
-  dest: './dist',
-  head: [['link', { rel: ' ', href: ' ' }]],
+  title: "Ailjc notes",
+  description: " ",
+  dest: "./dist",
+  head: [["link", { rel: " ", href: " " }]],
   markdown: {
     lineNumbers: true, // 代码块显示行号
     toc: {
-      includeLevel: [2, 3],
-    },
+      includeLevel: [2, 3]
+    }
   },
-  base: '/luvue/',
+  base: "/luvue/",
   // configureWebpack: {
   //   resolve: {
   //     alias: {
@@ -159,27 +112,27 @@ module.exports = {
 
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
-      { text: 'Js', link: '/Js/' },
-      { text: 'React', link: '/react/' },
-      { text: '快速掌握Vue', link: '/vuebase/' },
-      { text: 'Node', link: '/node/' },
-      { text: 'Typescript', link: '/typescript/' },
-      { text: 'Vue3', link: '/vue3/' },
-      { text: '源码分析', link: '/vuesourcecode/' },
-      { text: '组件库', link: '/lu-ui/' },
-      { text: '工程化', link: '/engineering/' },
+      { text: "首页", link: "/" },
+      { text: "Js", link: "/Js/" },
+      { text: "React", link: "/react/" },
+      { text: "快速掌握Vue", link: "/vuebase/" },
+      { text: "Node", link: "/node/" },
+      { text: "Typescript", link: "/typescript/" },
+      { text: "Vue3", link: "/vue3/" },
+      { text: "源码分析", link: "/vuesourcecode/" },
+      { text: "组件库", link: "/lu-ui/" },
+      { text: "工程化", link: "/engineering/" }
     ],
     sidebar: {
-      '/vuebase/': getSidebar('vuebase'),
-      '/node/': getSidebar('node'),
-      '/typescript/': getSidebar('typescript'),
-      '/vue3/': getSidebar('vue3'),
-      '/vuesourcecode/': getSidebar('vuesourcecode'),
-      '/lu-ui/': getSidebar('lu-ui'),
-      '/engineering/': getSidebar('engineering'),
-      '/Js/': getSidebar('Js'),
-      '/react/': getSidebar('react'),
-    },
-  },
+      "/vuebase/": getSidebar("vuebase"),
+      "/node/": getSidebar("node"),
+      "/typescript/": getSidebar("typescript"),
+      "/vue3/": getSidebar("vue3"),
+      "/vuesourcecode/": getSidebar("vuesourcecode"),
+      "/lu-ui/": getSidebar("lu-ui"),
+      "/engineering/": getSidebar("engineering"),
+      "/Js/": getSidebar("Js"),
+      "/react/": getSidebar("react")
+    }
+  }
 };
